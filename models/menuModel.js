fda5762f40653b7ceffc95a7e0f4a30f984db53d
Indhiro <indhiroelsaday@gmail.com>
2023-12-12 14:00:26 +0800

    update 3

--- models/menuModel.js
+++ models/menuModel.js
@@ -2,7 +2,7 @@ const con = require('../config/config');
 
 class menuModel {
     static getAllMenu(req, res, next) {
-        let query = `SELECT * FROM ${'`railway`'}.tb_menu
+        let query = `SELECT * FROM ${'`process.env.DATABASE`'}.menus
                     WHERE deleted_at IS NULL`
         con.query(query, function(err, result, fields) {
             if (err) {
@@ -14,7 +14,7 @@ class menuModel {
 
     static getMenu(req, res, next) {
         let id = req.body.id;
-        let query = `SELECT * FROM ${'`railway`'}.tb_menu WHERE id = ` + id
+        let query = `SELECT * FROM ${'`process.env.DATABASE`'}.menus WHERE id = ` + id
 
         con.query(query, function(err, result, fields) {
             if (err) res.status(400).send(err);
@@ -37,7 +37,7 @@ class menuModel {
         if (typeof stok != 'number') return res.send('Kolom stok harus number!');
         if (typeof deskripsi != 'string') return res.send('Kolom deskripsi harus string!');
 
-        let query = `INSERT INTO ${'`railway`'}.tb_menu SET 
+        let query = `INSERT INTO ${'`process.env.DATABASE`'}.menus SET 
                     nama = '${nama}', harga = '${harga}', kategori = '${kategori}', stok = '${stok}'`;
         con.query(query, function(err, result, fields) {
             if (err) {
@@ -70,7 +70,7 @@ class menuModel {
         if (typeof stok != 'number') return res.send('Kolom stok harus number!');
         if (typeof deskripsi != 'string') return res.send('Kolom deskripsi harus string!');
 
-        let query = `UPDATE ${'`railway`'}.tb_menu SET `;
+        let query = `UPDATE ${'`process.env.DATABASE`'}.menus SET `;
 
         if (nama) query += ` nama = '${nama}',`;
         if (harga) query += ` harga = '${harga}',`;
@@ -94,7 +94,7 @@ class menuModel {
 
         console.log(req);
 
-        let query = `UPDATE ${'`railway`'}.tb_menu SET deleted_at = ${deleted_at} 
+        let query = `UPDATE ${'`process.env.DATABASE`'}.menus SET deleted_at = ${deleted_at} 
                     WHERE id = ` + id
         query += ` AND deleted_at IS NULL`
 
