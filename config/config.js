7ce909522f229ceaed9ac4ddc3e9b1c170d1ba0a
Indhiro <indhiroelsaday@gmail.com>
2023-12-12 15:37:38 +0800

    PESAN

--- config/config.js
+++ config/config.js
@@ -1,17 +1,20 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "viaduct.proxy.rlwy.net",
-    user: "root",
-    password: "c4H2E3BD2hA5gG1EAfBCgdGF1hd6gD6E",
+    host: 'viaduct.proxy.rlwy.net',
+    user: 'root',
+    password: 'c4H2E3BD2hA5gG1EAfBCgdGF1hd6gD6E',
+    port: 40643,
     database: "railway"
 });
+
+console.log(con);
 
 con.connect(function (err) {
     if (err) throw err;
     let queryDb = `CREATE DATABASE IF NOT EXISTS db_restaurant`
     con.query(queryDb, function(err, result, fields) {
-        if (err) throw err;
+        if (err) console.log(2);
     });
     let queryTbMenu = `CREATE TABLE IF NOT EXISTS ${'`railway`'}.tb_menu (
         id int NOT NULL AUTO_INCREMENT,
@@ -26,7 +29,7 @@ con.connect(function (err) {
         PRIMARY KEY(id)
     )`
     con.query(queryTbMenu, function(err, result, fields) {
-        if (err) throw err;
+        if (err) console.log(3);
     });
     console.log("Database connected!");
 })
